dcc051a7cb6a40a66b5e3ade6f7fa0e3d738fea4
Gerardo Alvarez <70041405+Gerard9199@users.noreply.github.com>
2022-03-14 01:47:36 -0600

    Update settings.py

--- Alphonse_Services/settings.py
+++ Alphonse_Services/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd8^)n073xa8-1he86*iu@*$^q=&!6xkv$hgw*o5i*=t++3mt*)'
+SECRET_KEY = secretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
